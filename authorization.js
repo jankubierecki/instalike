bc529807a7f45500043c805861a1851c5a21dda1
Jan Kubierecki <jankubierecki@gmail.com>
2017-08-05 21:18:59 +0200

    posts and files services

--- authorization.js
+++ authorization.js
@@ -2,12 +2,13 @@ var authTokenClient = require('./clients/authTokenClient');
 
 module.exports = function (req, res, next) {
     if (req.path !== '/users/login' && req.path !== '/users/register') {
+        //TODO validate authorization header
         let apiToken = req.header('Authorization');
         authTokenClient.getAuthToken(apiToken, function (tokens) {
             let isAuthenticated = tokens.length !== 0;
             if (isAuthenticated) {
                 req.apiToken = apiToken;
-                req.userID = tokens[0].id;
+                req.userID = tokens[0].userID;
                 next();
             } else {
                 return res.sendStatus(403);
