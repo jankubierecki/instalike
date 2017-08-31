9f214108a11fa816a25986aca2c8560522c1f812
Jan Kubierecki <jankubierecki@gmail.com>
2017-08-31 21:56:39 +0200

    insert mysql changes into postClient;

--- clients/postClient.js
+++ clients/postClient.js
@@ -6,23 +6,27 @@ class PostClient {
         this.paginateBy = 10;
         this.fileUploadPath = path.join(path.dirname(path.dirname(require.main.filename)), '/uploads/posts/');
         this.createPostSQL = 'INSERT INTO `posts`( `userID`, `title`, `description`, `fotoPath`) VALUES (?, ?, ?, ?);';
-        this.getPostsForUsersSQL = 'SELECT posts.*, COUNT(comments.id) AS commentsCount  FROM `posts` ' +
-            'LEFT JOIN comments ON posts.id = comments.postID ' +
-            'WHERE posts.`userID` IN (?) ' +
-            'GROUP BY posts.id ' +
-            'ORDER BY `createdAt` DESC LIMIT ?,?;';
+        this.getPostsForUsersSQL = "SELECT posts.*, COUNT(DISTINCT comments.id) AS commentsCount, COUNT(DISTINCT posts_likes.id) AS likesCount FROM `posts` " +
+            "            LEFT JOIN comments ON posts.id = comments.postID" +
+            "            LEFT JOIN posts_likes ON posts.id = posts_likes.postID " +
+            "            WHERE posts.`userID` IN (?) " +
+            "            GROUP BY posts.id" +
+            "            ORDER BY `createdAt` DESC LIMIT ?,?;";
         this.updatePostSQL = 'UPDATE `posts` SET `title`= ?, `description`= ? WHERE id = ?;';
         this.deletePostSQL = 'DELETE FROM `posts` WHERE `id` = ?;';
-        this.getPostSQL = 'SELECT * FROM `posts` WHERE `id` = ?;';
+        this.getPostSQL = "SELECT posts.*, COUNT(DISTINCT comments.id) AS commentsCount, COUNT(DISTINCT posts_likes.id) AS likesCount FROM `posts` " +
+            "LEFT JOIN comments ON posts.id = comments.postID " +
+            "LEFT JOIN posts_likes ON posts.id = posts_likes.postID " +
+            "WHERE posts.id = ?";
         this.getPostCountSQL = 'SELECT COUNT(*) AS postCount FROM `posts` WHERE `userID` = ?;';
-        this.getPostFromFriendsSQL =
-            'SELECT posts.id, posts.userID, posts.title, posts.fotoPath, posts.description, posts.createdAt, users.email, COUNT(comments.id) AS commentsCount ' +
-            'FROM posts ' +
-            'INNER JOIN users ON posts.userID = users.id ' +
-            'LEFT JOIN comments ON posts.id = comments.postID ' +
-            'WHERE posts.userID IN (SELECT friendID FROM friends WHERE userID = ?) OR posts.userID = ? ' +
-            'GROUP BY posts.id ' +
-            'ORDER BY posts.createdAt DESC LIMIT ?,?';
+        this.getPostFromFriendsSQL = "SELECT posts.id, posts.userID, posts.title, posts.fotoPath, posts.description, posts.createdAt, users.email, COUNT(DISTINCT comments.id) AS commentsCount, COUNT(DISTINCT posts_likes.id) AS likesCount " +
+            "FROM posts " +
+            "INNER JOIN users ON posts.userID = users.id " +
+            "LEFT JOIN comments ON posts.id = comments.postID " +
+            "LEFT JOIN posts_likes ON posts.id = posts_likes.postID " +
+            "WHERE posts.userID IN (SELECT friendID FROM friends WHERE userID = ?) OR posts.userID = ? " +
+            "GROUP BY posts.id " +
+            "ORDER BY posts.createdAt DESC LIMIT ?,?;";
         this.isLatherThanSQL = "SELECT * FROM  comments WHERE createdAt >= (NOW() - INTERVAL 2 MINUTE);";
 
     }
